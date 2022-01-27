93a9e8f06c2e278ed7170eaf5449275f2c0198b3
XSener <91891551+XSener@users.noreply.github.com>
2022-01-27 21:30:24 +0100

    Add files via upload

--- webclient.cpp
+++ webclient.cpp
@@ -1,7 +1,35 @@
 #include "webclient.h"
+#include <QDebug>
+#include <QCoreApplication>
 
 Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+    m_socket = new QTcpSocket(this);
+
+    connect(m_socket, &QTcpSocket::connected, this, &Webclient::connected);
+    connect(m_socket, &QTcpSocket::readyRead, this, &Webclient::readyRead);
 }
+void Webclient::connected()
+{
+    m_socket->write(QString("GET / HTTP/1.1\r\nHost:" + m_hostname + "\r\n\r\n").toLocal8Bit());
+}
+
+void Webclient::readyRead()
+{
+    textBrowser->setText( m_socket->readAll());
+    m_socket->disconnectFromHost();
+
+}
+void Webclient::on_pushButton_clicked()
+{
+    textBrowser->clear();
+    m_hostname = hostname->text();
+
+    m_socket->connectToHost(m_hostname, m_port);
+    if (!m_socket->waitForConnected(5000)) {
+        textBrowser->setText("Connect failed!");
+    }
+}
+
--- webclient.h
+++ webclient.h
@@ -1,5 +1,7 @@
 #ifndef WEBCLIENT_H
 #define WEBCLIENT_H
+#include <QAbstractSocket>                                                  // für die Sockets
+#include <QTcpSocket>                                                       // für die Sockets
 
 #include "ui_webclient.h"
 
@@ -9,6 +11,16 @@ class Webclient : public QWidget, private Ui::Webclient
 
 public:
     explicit Webclient(QWidget *parent = nullptr);
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    QString m_hostname;
+    QTcpSocket *m_socket;
+    unsigned short m_port = 80;
+    void connected();
+    void readyRead();
+
 };
 
 #endif // WEBCLIENT_H
